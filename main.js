159c19d943f7181040925c77668a351ac5d8b967
not_spagheti <notspagheti@gmail.com>
2026-03-24 14:23:11 +0100

    Notebook -> PC sync

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const {app, BrowserWindow} = require("electron");
 
+const winSize = {width: 1600, height: 900}
+
 function createWindow() {
     const win = new BrowserWindow({
-        width: 1600,
-        height: 900
+        width: winSize.width,
+        height: winSize.height,
+        title: "PastaBrowse",
     });
 
-    win.loadFile("public/index.html");
+    const topbar = new BrowserWindow({
+        parent: win,
+        width: winSize.width,
+        height: winSize.height,
+    })
+
+    topbar.webContents.loadFile('./public/index.html');
+    topbar.setPosition(win.getPosition()[0], win.getPosition()[1]);
+
+    topbar.on('move', () => {
+        win.setPosition(topbar.getPosition()[0], topbar.getPosition()[1]);
+    });
 }
 
-app.whenReady().then(createWindow);
+app.on('ready', () => createWindow());
