a9be02c4fd6b4ebe3820cdb1b71ed44244031680
not_spagheti <notspagheti@gmail.com>
2026-05-11 20:32:56 +0200

    Add: logic for theme switching while using lin. grad. & !lin. grad.

--- public/settings_page/themes_page/themes_main.js
+++ public/settings_page/themes_page/themes_main.js
@@ -10,37 +10,37 @@ linearGradientCheck.addEventListener("change", (e) => {setLinearGradient()});
 
 
 const redBtn = document.getElementById("redBtn");
-redBtn.addEventListener("click", () => {setTheme("theme-red")});
+redBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-red-linear-gradient" : "theme-red")});
 
 const greenBtn = document.getElementById("greenBtn");
-greenBtn.addEventListener("click", () => {setTheme("theme-green")});
+greenBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-green-linear-gradient" : "theme-green")});
 
 const blueBtn = document.getElementById("blueBtn");
-blueBtn.addEventListener("click", () => {setTheme("theme-blue")});
+blueBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-blue-linear-gradient" : "theme-blue")});
 
 const orangeBtn = document.getElementById("orangeBtn");
-orangeBtn.addEventListener("click", () => {setTheme("theme-orange")});
+orangeBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-orange-linear-gradient" : "theme-orange")});
 
 const bronzeBtn = document.getElementById("bronzeBtn");
-bronzeBtn.addEventListener("click", () => {setTheme("theme-bronze")});
+bronzeBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-bronze-linear-gradient" : "theme-bronze")});
 
 const tealBtn = document.getElementById("tealBtn");
-tealBtn.addEventListener("click", () => {setTheme("theme-teal")});
+tealBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-teal-linear-gradient" : "theme-teal")});
 
 const indigoBtn = document.getElementById("indigoBtn");
-indigoBtn.addEventListener("click", () => {setTheme("theme-indigo")});
+indigoBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-indigo-linear-gradient" : "theme-indigo")});
 
 const purpleBtn = document.getElementById("purpleBtn");
-purpleBtn.addEventListener("click", () => {setTheme("theme-purple")});
+purpleBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-purple-linear-gradient" : "theme-purple")});
 
 const pinkBtn = document.getElementById("pinkBtn");
-pinkBtn.addEventListener("click", () => {setTheme("theme-pink")});
+pinkBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-pink-linear-gradient" : "theme-pink")});
 
 const greyBtn = document.getElementById("greyBtn");
-greyBtn.addEventListener("click", () => {setTheme("theme-grey")});
+greyBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-grey-linear-gradient" : "theme-grey")});
 
 const darkBtn = document.getElementById("darkBtn");
-darkBtn.addEventListener("click", () => {setTheme("theme-dark")});
+darkBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-dark-linear-gradient" : "theme-dark")});
 
 const fGreenBtn = document.getElementById("fGreenBtn");
-fGreenBtn.addEventListener("click", () => {setTheme("theme-forest-green")});
+fGreenBtn.addEventListener("click", () => {setTheme(linearGradientCheck.checked ? "theme-forest-green-linear-gradient" : "theme-forest-green")});
