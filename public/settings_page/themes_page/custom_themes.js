2be0ad684a9943b87645e5da3b74bfe274251c91
notspagheti <notspagheti@gmail.com>
2026-05-21 08:17:51 +0200

    Add: Custom Theme Config close button

--- public/settings_page/themes_page/custom_themes.js
+++ public/settings_page/themes_page/custom_themes.js
@@ -8,7 +8,21 @@ export function openCustomThemeConfig(e){
     customThemeConfig.remove();
   };
 
-  const html = `<div id="customThemeConfig_${cThemeId}" class="custom_theme_config"><p>${cThemeId}</p></div>`;
+  const html = `
+  <div id="customThemeConfig_${cThemeId}" class="custom_theme_config">
+    <div class="topbar">
+      <h3>Custom Theme ${cThemeId}</h3>
+      <button id="closeCustomThemeConfigBtn" class="">X</button>
+    </div>
+  </div>`;
 
   themeSettings.insertAdjacentHTML("afterend", html);
+
+  const closeCustomThemeConfigBtn = document.getElementById("closeCustomThemeConfigBtn");
+  closeCustomThemeConfigBtn.addEventListener("click", closeCustomThemeConfig)
+}
+
+
+function closeCustomThemeConfig(e){
+  e.target.parentElement.parentElement.remove();
 }
