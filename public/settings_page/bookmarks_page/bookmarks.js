851fc5f944ac2ad3683ba6ecb4a98c3dcc27ed3d
notspagheti <notspagheti@gmail.com>
2026-06-25 10:51:46 +0200

    Add: bookmark UI && Fix: theme loading

--- public/settings_page/bookmarks_page/bookmarks.js
+++ public/settings_page/bookmarks_page/bookmarks.js
@@ -3,3 +3,17 @@ let bookmarks = {};
 export function setBookmark(key, value){
   bookmarks[key] = value;
 }
+
+
+export function addBookmarkUI(key, value){
+  const template = document.createElement("div");
+  const html = `
+    <h2 class="title">${key}</h2>
+    <p>${value}</p>`;
+
+
+  template.classList.add("bookmark");
+  template.innerHTML = html;
+
+  document.body.appendChild(template);
+}
--- public/settings_page/bookmarks_page/main.js
+++ public/settings_page/bookmarks_page/main.js
@@ -1,5 +1,19 @@
-import {setSettingsTabId, onResCustomThemeCss, onResSettings, getBookmarks, onResBookmarks} from "../ipc.js";
-import {setBookmark} from "./bookmarks.js";
+import {setSettingsTabId, onResCustomThemeCss, onResSettings, getBookmarks, onResBookmarks, getCustomThemeCss} from "../ipc.js";
+import {setBookmark, addBookmarkUI} from "./bookmarks.js";
+
+
+function injectCss(css){
+  let el = document.getElementById("injected-custom-theme");
+    
+  if (!el){
+    el = document.createElement("style");
+    el.id = "injected-custom-theme";
+    document.head.appendChild(el);
+  }
+  
+  el.textContent = css;
+}
+
 
 setSettingsTabId("bookmarks");
 
@@ -18,18 +32,8 @@ onResSettings((settings) => {
 getBookmarks();
 
 onResBookmarks((bookmarks) => {
-  Object.keys(bookmarks).forEach((key) => {setBookmark(key, bookmarks[key])})
+  Object.keys(bookmarks).forEach((key) => {
+    setBookmark(key, bookmarks[key]);
+    addBookmarkUI(key, bookmarks[key]);
+  })
 })
-
-
-function injectCss(css){
-  let el = document.getElementById("injected-custom-theme");
-    
-  if (!el){
-    el = document.createElement("style");
-    el.id = "injected-custom-theme";
-    document.head.appendChild(el);
-  }
-  
-  el.textContent = css;
-}
