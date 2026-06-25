441487579dd00442716beefd800eb83a67db039f
notspagheti <notspagheti@gmail.com>
2026-06-25 11:45:08 +0200

    Add: favicons in bookmark UI

--- public/settings_page/bookmarks_page/bookmarks.js
+++ public/settings_page/bookmarks_page/bookmarks.js
@@ -8,12 +8,20 @@ export function setBookmark(key, value){
 export function addBookmarkUI(key, value){
   const template = document.createElement("div");
   const html = `
-    <h2 class="title">${key}</h2>
-    <p>${value}</p>`;
+    <img alt="" id="img_${value}">
+    <div class="text_container">
+      <h2 class="title">${key}</h2>
+      <p>${value}</p>
+    </div>`;
 
 
   template.classList.add("bookmark");
   template.innerHTML = html;
 
   document.body.appendChild(template);
+
+  const img = document.getElementById(`img_${value}`);
+  const favIconSize = 64;
+  
+  img.src = `https://www.google.com/s2/favicons?domain=${value}&sz=${String(favIconSize)}`;
 }
