691eb439dcf272ed060ea6c59638a99ce0409bd8
notspagheti <notspagheti@gmail.com>
2026-07-30 21:06:12 +0200

    Add: Downloads page

---
+++ public/settings_page/downloads_page/main.js
@@ -0,0 +1,29 @@
+import {setSettingsTabId, onResCustomThemeCss, onResSettings, getCustomThemeCss} from "../ipc.js";
+
+
+function injectCss(css){
+  let el = document.getElementById("injected-custom-theme");
+    
+  if (!el){
+    el = document.createElement("style");
+    el.id = "injected-custom-theme";
+    document.head.appendChild(el);
+  }
+  
+  el.textContent = css;
+}
+
+
+setSettingsTabId("downloads");
+
+onResCustomThemeCss((css) => {
+  injectCss(css);
+})
+
+onResSettings((settings) => {
+  document.documentElement.classList = settings["theme"];
+
+  if (settings["theme"].includes("custom")){
+    getCustomThemeCss();
+  }
+})
