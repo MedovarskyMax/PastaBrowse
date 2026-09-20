e66423b65331b605457f3b17a826089f6f62fce1
notspagheti <notspagheti@gmail.com>
2026-09-20 20:09:52 +0200

    Update: saves to designated dir instead of always prompting user

--- src/main.js
+++ src/main.js
@@ -130,6 +130,10 @@ function createWindow() {
     win.webContents.send("settings-preload-path", settings_preload_path);
     win.webContents.send("downloads", downloads) // send to renderer, rewrite downstream logic to only query the renderer copy
   })                         // only read/write to json at app launch and termination
+
+  win.webContents.session.on("will-download", (event, item, webContents) => {
+    item.setSavePath(path.join(downloads["downloadsPath"], item.getFilename()))
+  })
 }
 
 
