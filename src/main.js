b36aad6818094bcc2a05dd7c67c3fbd21dc388f0
notspagheti <notspagheti@gmail.com>
2026-09-21 21:37:25 +0200

    Add: electron will download boilerplate && create downloadObj on start download

--- src/main.js
+++ src/main.js
@@ -134,7 +134,42 @@ function createWindow() {
   win.webContents.session.on("will-download", (event, item, webContents) => {
     item.setSavePath(path.join(downloads["downloadsPath"], item.getFilename()));
     win.webContents.send("started-download");
+
+    const downloadObj = createDownloadObj(item);
+
+    item.on("updated", (event, state) => {
+      if (state === "interrupted"){
+        console.log("Download is interrupted but can be resumed")
+      } else if (state === "progressing"){
+        if (item.isPaused()){
+          console.log("Download is paused")
+        } else {
+          console.log(`Recieved bytes: ${item.getReceivedBytes()}`)
+        }
+      }
+    })
+
+    item.once("done", (event, state) => {
+      if (state === "completed"){
+        console.log("Download Successful")
+        console.log(downloadObj)
+      } else {
+        console.log(`Download failed: ${state}`)
+      }
+    })
   })
+}
+
+
+function createDownloadObj(item){
+  const tempDate = new Date();
+
+  return {
+    "fileName": item.getFilename(),
+    "savePath": item.getSavePath(),
+    "date": tempDate.toLocaleDateString("en-US", {dateStyle: "long"}),
+    "icon": "downloadInProgress"
+  }
 }
 
 
